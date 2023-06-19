44f57b935ed0b78de8e763523c9357dbb250dc3e
Anderson Lorena <anderson_datadyne@hotmail.com>
2023-06-19 18:28:20 -0300

    created spring boot project scaffolding.

---
+++ src/main/java/com/andersonlorena/springboot/apilab/ApiLabApplication.java
@@ -0,0 +1,13 @@
+package com.andersonlorena.springboot.apilab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andersonlorena/springboot/apilab/ApiLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andersonlorena.springboot.apilab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
